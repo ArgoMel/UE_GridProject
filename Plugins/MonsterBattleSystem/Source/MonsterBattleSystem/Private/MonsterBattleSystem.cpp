fb39bd6dba41e64db5a7d68e0bd9a432f23b9673
ArgoMel <qex1357@naver.com>
2025-03-19 19:02:20 +0900

    Initial commit

---
+++ Plugins/MonsterBattleSystem/Source/MonsterBattleSystem/Private/MonsterBattleSystem.cpp
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MonsterBattleSystem.h"
+
+#define LOCTEXT_NAMESPACE "FMonsterBattleSystemModule"
+
+void FMonsterBattleSystemModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FMonsterBattleSystemModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FMonsterBattleSystemModule, MonsterBattleSystem)
---
+++ Source/GridProject.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class GridProjectTarget : TargetRules
+{
+	public GridProjectTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		ExtraModuleNames.Add("GridProject");
+	}
+}
